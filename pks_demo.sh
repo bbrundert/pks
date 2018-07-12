10593d24de29a89c1df17729e3761ec3a8c40974
Bjoern Brundert <bbjoern@gmail.com>
2018-07-12 14:32:25 +0200

    added SERVICEINSTANCE variable

--- pks_demo.sh
+++ pks_demo.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#######
+# might require some tools to be installed:
+# 
+# pv: http://www.ivarch.com/programs/pv.shtml
+#
+# sudo apt-get install pv
+# brew install pv
+#
 ########################
 # include the magic
 ########################
@@ -12,5 +20,10 @@ clear
 pe "pks login -a pks.corp.local -u admin -p MXoIThjoSxm2NBVNOh_AjKm5lgvzQH6l -k"
 pe "pks clusters"
 pe "pks cluster my-cluster"
+p "pks create-cluster my-kube -e 10.40.14.34 -n 1"
+pe "bosh -e kubobosh deployments"
+pe "bosh -e kubobosh deployments | grep service-instance_* | awk '{print $1}'"
+SERVICEINSTANCE=$(bosh -e kubobosh deployments | grep service-instance_* | awk '{print $1}')
+pe "bosh -e kubobosh -d $SERVICEINSTANCE instances"
 
 #test
